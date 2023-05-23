9517909ddac1f233c04fc03634b79cd9b8c358ae
Andrey Pyanykh <>
2023-05-24 00:25:53 +0500

    add pytest_addoption in conftest.py

--- tests/conftest.py
+++ tests/conftest.py
@@ -3,13 +3,22 @@ from appium import webdriver
 from appium.options.android import UiAutomator2Options
 
 
+def pytest_addoption(parser):
+    parser.addoption('--udid', action='store',
+                     help="Choose udid device")
+    parser.addoption('--url', action='store', default='http://127.0.0.1:4723',
+                     help="Choose url appium")
+
+
 @pytest.fixture(scope="function")
-def driver():
+def driver(request):
+    udid = request.config.getoption("udid")
+    url = request.config.getoption("url")
     options = UiAutomator2Options()
     options.platform_name = 'Android'
-    options.udid = '192.168.31.60:5555'
+    options.udid = udid
     options.newCommandTimeout = 3600
-    driver = webdriver.Remote('http://127.0.0.1:4723', options=options)
+    driver = webdriver.Remote(url, options=options)
     driver.unlock()
 
     yield driver
